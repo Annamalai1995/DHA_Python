231054300ff25617dac61ff52bba667d6d0b9ee6
Annamalai1995 <s.annamalai1995@gmail.com>
2026-04-22 11:19:11 +0530

    castinganduserinput

---
+++ casting.py
@@ -0,0 +1,15 @@
+casting=450
+print(type(casting))
+hello=float(casting)
+print(hello)
+
+
+age=20
+print(type(age))
+hai=str(age)
+print(type(hai))
+
+
+salary=float(input("Enter income"))
+print("Salary",salary)
+print(int(salary))
---
+++ userinput.py
@@ -0,0 +1,11 @@
+#input 
+alpha=int(input("Enter value="))
+beta=int(input("Enter value="))
+result=alpha+beta
+print("The sum of num",result)
+
+
+Name=input("Enter name")
+print("name is ",Name)
+
+
