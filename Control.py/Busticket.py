de666399c77394d7d2fc7bf3caec1a8bcf577c53
Annamalai1995 <s.annamalai1995@gmail.com>
2026-04-29 11:20:10 +0530

    control statements

---
+++ Control.py/Busticket.py
@@ -0,0 +1,24 @@
+# age=int(input("enter the AGe"))
+# if age<=10:
+#     print("Half Ticket")
+# elif age >18:
+#     print("Full Ticket")
+# elif age > 60:
+#     print("Senior citizen ticket free")        
+# else:
+#     print("Full ticket")
+
+
+
+#THEMEPARK
+Age=int(input("enter The Age"))
+if Age >=18:
+    print("Welcome to wonderla Bangalore")
+    print("Thrilling and water games welcome")
+    gender=input("Enter The gender")
+    if gender =="female" or gender=="male":
+        print("Water games")
+    else:
+        print("Dry games") 
+else:
+    print("GO OUT BYE BYE")          
