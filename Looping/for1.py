58d78c44bf3bd71d77050c06c11eccf2d1355aee
Annamalai1995 <s.annamalai1995@gmail.com>
2026-04-30 11:20:27 +0530

    looping

---
+++ Looping/for1.py
@@ -0,0 +1,37 @@
+1 < 5
+0
+1 
+2 
+3 
+4
+
+#FOR:
+for name in "Dhanya":
+    print(name)
+
+#Orders
+# order=[200,500,200,150,400]
+# total=0
+# for price in order:
+#     total+=price
+
+# print("Total",total)   
+
+
+#EVen Number
+# even=[2,10,15,16,18,30,50,1]
+# for a in even:
+#     if a%2==0:
+#         print("Even number ",a)
+#     else:
+#         print("ODD number",a)    
+
+
+#FOR IN RANGE
+for hello in range(1,10):
+    print(hello)
+#Tables:
+table=int(input("Enter Table Number"))
+for i in range(1,11):
+    print(f"{table}*{i}={table*i}")
+    
