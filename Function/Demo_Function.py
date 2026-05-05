00ade3dc4d139bd5b456497cd35890993d435f37
Annamalai1995 <s.annamalai1995@gmail.com>
2026-05-05 11:09:07 +0530

    functions

---
+++ Function/Demo_Function.py
@@ -0,0 +1,24 @@
+def Aiswarya():
+    print("550 Marks")
+Aiswarya()   
+# with return
+
+def add():
+    alpha=150
+    beta=300
+    sum=alpha+beta
+    return sum
+print("The sum of numbers",add())  
+
+#without return
+def sum():
+    alpha=100
+    beta=580
+    add=alpha+beta
+    print("The sum ",add)
+sum()
+#With parameter
+def college(clgname):
+    print("sona",clgname)
+college("College")    
+       
