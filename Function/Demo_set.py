dfbd94de0df5e6c4a9ae55cbcfe109b375804357
Annamalai1995 <s.annamalai1995@gmail.com>
2026-05-15 11:05:53 +0530

    dICT

--- Function/Demo_set.py
+++ Function/Demo_set.py
@@ -15,3 +15,34 @@ a={'Coorg','Manali','Ooty','Kodaikanal'}
 li=['Hari']
 a.update(li)
 print(a)
+
+#remove Set
+a={1,2,3,"sam","pk"}
+a.remove("sam")
+print(a)
+a.discard(2)
+
+print(a)
+
+ 
+#pop method
+s={"sam","pk","sathish","viji" }
+s1=s.pop()
+print(s1)
+print(s)
+
+
+
+#Access Set
+a={"sam","pk","anu","sathish"}
+for i in a:
+    print(i)
+
+#check the set is present or not
+a={"sam","pk","anu","sathish"}
+print("pk" in a)
+
+
+
+
+
